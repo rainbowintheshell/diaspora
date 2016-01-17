83449d1964750560c30dd4ce5d31f405197dcd3d
Christian Gimenez <cgimenez.public@openmailbox.org>
2016-01-17 19:07:29 -0300

    Quitar la barra progreso de "donate"

--- app/helpers/motd_helper.rb
+++ app/helpers/motd_helper.rb
@@ -4,13 +4,13 @@
 
 module MotdHelper
   def donationWidth()
-    uri = URI.parse("https://zauberstuhl.de/donate/json")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    res = http.get(uri.request_uri)
-
-    (JSON.parse(res.body))['width']
-  rescue
+#    uri = URI.parse("https://zauberstuhl.de/donate/json")
+#    http = Net::HTTP.new(uri.host, uri.port)
+#    http.use_ssl = true
+#    res = http.get(uri.request_uri)
+#
+#    (JSON.parse(res.body))['width']
+#  rescue
     0
   end
 end
